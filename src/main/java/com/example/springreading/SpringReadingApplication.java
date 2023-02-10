5967a522e30475861cf19688a71b04fbc85b74cc
Wang junwei <wangjunwei@2woniu.com>
2023-02-10 14:54:36 +0800

    初始化

---
+++ src/main/java/com/example/springreading/SpringReadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReadingApplication.class, args);
+	}
+
+}
