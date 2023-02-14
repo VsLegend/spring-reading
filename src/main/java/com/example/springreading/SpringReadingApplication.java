77db567af5c4fee386490474ed0b7175e8831498
Wang junwei <wangjunwei@2woniu.com>
2023-02-14 18:03:31 +0800

    pom设置

--- src/main/java/com/example/springreading/SpringReadingApplication.java
+++ src/main/java/com/example/springreading/SpringReadingApplication.java
@@ -1,5 +1,7 @@
 package com.example.springreading;
 
+import com.example.springreading.service.BusinessService;
+import com.example.springreading.service.BusinessServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,8 @@ public class SpringReadingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReadingApplication.class, args);
+		BusinessServiceImpl businessService = new BusinessServiceImpl(BusinessServiceImpl.class.getName());
+		System.out.println(businessService.getName());
 	}
 
 }
